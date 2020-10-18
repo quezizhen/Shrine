854d1fd877c97c1ed2f71ed445c1af5a93ede6a7
shanheliu <quezizhen@sina.com>
2020-10-18 21:16:49 +0800

    Add products Cards in home page

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -13,6 +13,10 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/products_repository.dart';
+import 'model/product.dart';
 
 class HomePage extends StatelessWidget {
   // TODO: Make a collection of cards (102)
@@ -22,46 +26,106 @@ class HomePage extends StatelessWidget {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
-      // TODO: Add app bar (102)
-      appBar: AppBar(
-        // TODO: Add buttons and title (102)
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-            semanticLabel: 'menu',
+        // TODO: Add app bar (102)
+        appBar: AppBar(
+          // TODO: Add buttons and title (102)
+          leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              semanticLabel: 'menu',
+            ),
+            onPressed: () {
+              print('Menu button');
+            },
           ),
-          onPressed: () {
-            print('Menu button');
-          },
+          title: Text('SHRINE'),
+          // TODO: Add trailing buttons (102)
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                semanticLabel: 'search',
+              ),
+              onPressed: () {
+                print('Search button');
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.tune,
+                semanticLabel: 'filter',
+              ),
+              onPressed: () {
+                print('Filter button');
+              },
+            ),
+          ],
         ),
-        title: Text('SHRINE'),
-        // TODO: Add trailing buttons (102)
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.search,
-              semanticLabel: 'search',
+        // TODO: Add a grid view (102)
+        body: GridView.count(
+            crossAxisCount: 2,
+            padding: EdgeInsets.all(16.0),
+            childAspectRatio: 8.0 / 9.0,
+            // TODO: Build a grid of cards (102)
+            children: _buildGridCards(context))
+        // TODO: Set resizeToAvoidBottomInset (101)
+        );
+  }
+
+  // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
+      return Card(
+        // TODO: Adjust card heights (103)
+        child: Column(
+          // TODO: Center items on the card (103)
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18 / 11,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                // TODO: Adjust the box size (102)
+                fit: BoxFit.fitWidth,
+              ),
             ),
-            onPressed: () {
-              print('Search button');
-            },
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.tune,
-              semanticLabel: 'filter',
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  // TODO: Align labels to the bottom and center (103)
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // TODO: Change innermost Column (103)
+                  children: <Widget>[
+                    // TODO: Handle overflowing labels (103)
+                    Text(
+                      product.name,
+                      style: theme.textTheme.title,
+                      maxLines: 1,
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      formatter.format(product.price),
+                      style: theme.textTheme.body2,
+                    ),
+                  ],
+                ),
+              ),
             ),
-            onPressed: () {
-              print('Filter button');
-            },
-          ),
-        ],
-      ),
-      // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
-      ),
-      // TODO: Set resizeToAvoidBottomInset (101)
-    );
+          ],
+        ),
+      );
+    }).toList();
   }
 }
